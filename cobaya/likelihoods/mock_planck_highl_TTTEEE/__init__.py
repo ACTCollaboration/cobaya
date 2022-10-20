6c549a205d790bf85d22bdd58c6ec4a1ffba6a33
Mathew Madhavacheril <mathewsyriac@gmail.com>
2022-10-20 10:07:36 -0400

    mock planck cobaya likelihoods with a hard-coded path unfortunately

---
+++ cobaya/likelihoods/mock_planck_highl_TTTEEE/__init__.py
@@ -0,0 +1,5 @@
+from cobaya.likelihoods.base_classes import CMBlikes
+
+
+class mock_planck_highl_TTTEEE(CMBlikes):
+    pass
---
+++ cobaya/likelihoods/mock_planck_lowl_TT/__init__.py
@@ -0,0 +1,5 @@
+from cobaya.likelihoods.base_classes import CMBlikes
+
+
+class mock_planck_lowl_TT(CMBlikes):
+    pass

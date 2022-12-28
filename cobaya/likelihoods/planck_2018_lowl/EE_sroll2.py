8c3fac18f735429529ffeaefee9a4cd4b4c0bf29
Mathew Madhavacheril <mathewsyriac@gmail.com>
2022-12-28 12:28:01 -0500

    sroll2 likelihood with hardcoded paths for now

---
+++ cobaya/likelihoods/planck_2018_lowl/EE_sroll2.py
@@ -0,0 +1,8 @@
+from cobaya.likelihoods.base_classes import Planck2018Clik
+
+
+class EE_sroll2(Planck2018Clik):
+    r"""
+    Low-$\ell$ EE likelihood of Planck's 2018 data release \cite{Aghanim:2019ame}.
+    """
+    pass
